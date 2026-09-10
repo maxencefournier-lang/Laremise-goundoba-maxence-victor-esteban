a9f86b5df42a79a8e957cd6432f26f7b2710a439
yocswar_perez <yocswar29@gmail.com>
2026-09-10 14:20:38 +0200

    feat(stats): handle dashboard endpoint errors

--- server/routes/stats.js
+++ server/routes/stats.js
@@ -4,31 +4,40 @@ import { pool } from "../db.js";
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-
-    const statuts = await pool.query(`
+    try {
+        // nombre d'objets par statut
+        const statuts = await pool.query(`
         SELECT statut, COUNT(*) AS nombre
         FROM objet
         GROUP BY statut
     `);
 
-    // Poids total reçu
-    const poidsTotal = await pool.query(`
+        // Poids total reçu
+        const poidsTotal = await pool.query(`
         SELECT SUM(poids_kg) AS total
         FROM objet
     `);
 
-    // Poids détourné de la déchetterie
-    const poidsDetourne = await pool.query(`
+        // Poids détourné de la déchetterie
+        const poidsDetourne = await pool.query(`
         SELECT SUM(poids_kg) AS total
         FROM objet
         WHERE statut != 'recycle'
     `);
 
-    res.status(200).json({
-        objets_par_statut: statuts.rows,
-        poids_total_recu: poidsTotal.rows[0].total,
-        poids_detourne: poidsDetourne.rows[0].total
-    });
+        res.status(200).json({
+            objets_par_statut: statuts.rows,
+            poids_total_recu: poidsTotal.rows[0].total,
+            poids_detourne: poidsDetourne.rows[0].total,
+        });
+    } catch (error) {
+        console.error(error);
+
+        return res.status(500).json({
+            error: "Errour interne du serveur",
+        });
+    }
 });
 
+router.get;
 export default router;
