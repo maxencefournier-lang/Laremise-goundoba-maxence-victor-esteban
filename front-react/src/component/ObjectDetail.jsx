9f67d09ced47c0eb9ff4b4ed48a371ee09fa49a1
Goundoba Diaby <goundoba.diaby2403@gmail.com>
2026-09-15 16:05:35 +0200

    modif composant  ObjectDetail

--- front-react/src/component/ObjectDetail.jsx
+++ front-react/src/component/ObjectDetail.jsx
@@ -8,7 +8,7 @@ export function ObjectDetail({libelle, numero_depot, etat_arrivee, poids_kg, cat
             <p>{libelle}</p>
             <p>N° du Depot : {numero_depot}</p>
             <p>Etat à l'arrivée : {etat_arrivee}</p>
-            <p>Poids : {poids_kg}</p>
+            <p>Poids : {poids_kg} kg</p>
             <p>Categorie : {categorie}</p>
             <p>Statut : {statut} </p>
         </article>
