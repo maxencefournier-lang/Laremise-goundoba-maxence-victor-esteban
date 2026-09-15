52adbd8a11ce8b12ce7d2e49af2edb5cfd0c3f0b
Maxence Fournier <maxence.fournier2309@gmail.com>
2026-09-15 18:05:45 +0200

    premier commit de l'ajout du nom de l'utilisateur sur dashboard

--- front-react/components/Card/Card.jsx
+++ front-react/components/Card/Card.jsx
@@ -1,26 +1,27 @@
-import { useEffect, useState } from "react"
-import { NavLink } from "react-router-dom"
+import { useNavigate } from "react-router-dom";
 
 function UserCard({ benevole }) {
-    
+    const navigate = useNavigate();
+
+    function seConnecter() {
+        localStorage.setItem("benevoleConnecte", JSON.stringify(benevole));
+
+        navigate("/dashboard");
+    }
+
     return (
         <div className="card">
-
             <div className="img"></div>
 
             <span className="name">
                 {benevole.prenom} {benevole.nom}
             </span>
 
-            <button onClick={() => console.log(benevole)}>
-                
-                <NavLink className="buttonBenev" to="/dashboard">
+            <button onClick={seConnecter}>
                 Se connecter
-                </NavLink>
             </button>
-
         </div>
-    )
+    );
 }
 
-export default UserCard
+export default UserCard;
