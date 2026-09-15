357acaed35a2b396af9d02355ba7338a2fdc6f01
yocswar_perez <yocswar29@gmail.com>
2026-09-15 16:02:13 +0200

    feat: add shared header to application layout

---
+++ front-react/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import "./Header.css";
+
+export default function Header() {
+    return (
+        <header className="header">
+            <span>LA REMISE</span>
+            <span>UTILISATEUR</span>
+        </header>
+    );
+}
